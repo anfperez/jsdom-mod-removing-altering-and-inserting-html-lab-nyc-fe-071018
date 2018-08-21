a53bb8e5b223e360922f9e97104ecbf1d2055368
Angela Perez <angela.perez@flatironschool.com>
2018-08-21 21:48:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ document.querySelector("main#main").remove();
 /* Create your new element here and assign it to newHeader */
 const newHeader = document.createElement("h1");
 
-newHeader.id = "victory"
+newHeader.id = "victory";
 newHeader.innerText = "Angela is the champion!";
 newHeader.className = "victory whatever";
