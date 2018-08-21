0e0fa0b83f6cdecb8764ce967cc19c7b7ac4790d
Angela Perez <angela.perez@flatironschool.com>
2018-08-21 21:42:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 /* Enter the code to remove the main node element under this comment */
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+const testVar = "hi"
+
+/* Enter the code to remove the node element here */
+document.querySelector("main#main").remove();
+
+/* Create your new element here and assign it to newHeader */
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerText = "Angela is the champion!";
+newHeader.className = "victory whatever";
